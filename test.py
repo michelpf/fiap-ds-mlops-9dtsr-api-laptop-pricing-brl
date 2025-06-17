c70a760dae6249f1717b5b9b717109fb2a3015c8
Michel Fernandes <michelpf@gmail.com>
2025-06-16 22:12:09 -0300

    Add initial implementation of model downloader and metadata storage
    - Create data.json with laptop specifications
    - Implement model_downloader.py to download the latest model and save metadata
    - Add requirements.txt for project dependencies
    - Create test.py to test the model handler functionality

---
+++ test.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+if os.path.exists("src/app.py"):
+    import src.app as app
+else:
+     import app
+
+with open("data.json", "r") as data: 
+     data = data.read()
+
+event = json.loads(data)
+
+print(event)
+
+retorno = app.handler(event, context=False)
+
+print(retorno)
+
+      
